e45ec7c1165c95d46ea48e9cd026fb8c84f218f2
Kavya Mantena <newtechkn@kavyas-air.lan>
2022-02-25 12:29:35 -0600

    started to create function generatePassword

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,17 @@
 // Assignment code here
 
+function generatePassword() {
+  var promptPass = window.prompt("How many characters would you like your password to contain");
 
+  if (promptPass <= 128) {
+    window.alert("Click OK to confirm including special characters");
+
+  } else {
+    window.alert("Please select characters length between 8 and 128 characters");
+    generatePassword();
+  }
+
+};
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
