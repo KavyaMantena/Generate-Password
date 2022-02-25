41a8ed44ecd88e3aa5231f81a9c56f98b0aab68e
Kavya Mantena <newtechkn@kavyas-air.lan>
2022-02-25 13:19:10 -0600

    created var type variables

--- Develop/script.js
+++ Develop/script.js
@@ -3,13 +3,21 @@
 function generatePassword() {
   var promptPass = window.prompt("How many characters would you like your password to contain");
 
-  if (promptPass <= 128) {
-    window.alert("Click OK to confirm including special characters");
-
-  } else {
+  if (promptPass > 128) {
     window.alert("Please select characters length between 8 and 128 characters");
     generatePassword();
   }
+
+
+  var pickedSpecial = window.confirm("Click OK to confirm including special characters");
+
+  var pickedNumeric = window.confirm("Click OK to confirm including numeric characters");
+
+
+  var pickedUpper = window.confirm("click OK to confirm including Uppercase.");
+
+  var pickedLower = window.confirm("click OK to confirm including Lowercase.");
+
 
 };
 // Get references to the #generate element
